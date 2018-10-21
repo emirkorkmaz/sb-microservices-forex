fba22c1b22074a2e3df56255ca913fa7b66d0b82
Emir Korkmaz <emir.korkmaz@payrov.com.tr>
2018-10-21 19:31:13 +0300

    initial

---
+++ src/main/java/com/study/sb/microservices/forex/sbmicroservicesforex/SbMicroservicesForexApplication.java
@@ -0,0 +1,12 @@
+package com.study.sb.microservices.forex.sbmicroservicesforex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMicroservicesForexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMicroservicesForexApplication.class, args);
+	}
+}
---
+++ src/test/java/com/study/sb/microservices/forex/sbmicroservicesforex/SbMicroservicesForexApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.sb.microservices.forex.sbmicroservicesforex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbMicroservicesForexApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
